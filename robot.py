27133551cc2927c9d303b0040f6ddcf03cd7580f
eviatarbutin <eviatar2004@gmail.com>
2020-01-29 20:47:29 +0200

    adding the robot class

---
+++ robot.py
@@ -0,0 +1,32 @@
+import magicbot
+import wpilib
+from components.shooter import Shooter
+from components.push_ball import Pusher
+from ctre import WPI_TalonSRX
+from wpilib import Joystick
+
+
+class MyRobot(magicbot):
+    shooter: Shooter
+    pusher: Pusher
+
+    def createObjects(self):
+        self.shooter.master_shooter_motor = WPI_TalonSRX(7)
+        self.shooter.slave_shooter_motor = WPI_TalonSRX(8)
+        self.joystick = Joystick(0)
+
+    def teleopPeriodic(self):
+        if self.Joystick.getTriggerReleased():
+            self.shooter.execute()
+
+
+
+
+
+
+
+
+
+
+
+
